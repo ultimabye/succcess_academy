6d13fa790a593011c6f90613e9e39aac38b7ca27
maaz56 <ultimabytes@gmail.com>
2023-01-31 16:00:53 +0500

    Password  changed

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,11 @@
 <?php
 $sname = "localhost";
 $uname = "root";
-$password = "1122";
+$password = "1234";
 $db_name = "successor_db";
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
-
+    
 if(!$conn){
     echo "Connection Failed";
     exit();
